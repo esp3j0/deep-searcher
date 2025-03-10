6d25a08f960d248bd17fa966df9b9573dd2590ad
zhangbaichuan <zhangbaichuan@forensix.cn>
2025-03-10 13:47:33 +0800

    设置llm config

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -9,8 +9,9 @@ httpx_logger = logging.getLogger("httpx")  # disable openai's logger output
 httpx_logger.setLevel(logging.WARNING)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
+ # replace with your openai api key
 config = Configuration()  # Customize your config here
+config.set_provider_config("llm", "SiliconFlow", {"model": "Pro/deepseek-ai/DeepSeek-V3"})
 init_config(config=config)
 
 
