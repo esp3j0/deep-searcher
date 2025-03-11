176e9a178b7dc8fdc09897ab4ec11e145d611d44
zhangbaichuan <zhangbaichuan@forensix.cn>
2025-03-11 11:32:46 +0800

    修改问题描述

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -19,19 +19,19 @@ init_config(config=config)
 # You should clone the milvus docs repo to your local machine first, execute:
 # git clone https://github.com/milvus-io/milvus-docs.git
 # Then replace the path below with the path to the milvus-docs repo on your local machine
-# import glob
-# all_md_files = glob.glob('xxx/milvus-docs/site/en/**/*.md', recursive=True)
-# load_from_local_files(paths_or_directory=all_md_files, collection_name="milvus_docs", collection_description="All Milvus Documents")
+import glob
+all_md_files = glob.glob('xxx/milvus-docs/site/en/**/*.md', recursive=True)
+load_from_local_files(paths_or_directory=all_md_files, collection_name="milvus_docs", collection_description="All Milvus Documents")
 
 # Hint: You can also load a single file, please execute it in the root directory of the deep searcher project
-load_from_local_files(
-    paths_or_directory=os.path.join(current_dir, "data/WhatisMilvus.pdf"),
-    collection_name="milvus_docs",
-    collection_description="All Milvus Documents",
-    # force_new_collection=True, # If you want to drop origin collection and create a new collection every time, set force_new_collection to True
-)
+# load_from_local_files(
+#     paths_or_directory=os.path.join(current_dir, "data/WhatisMilvus.pdf"),
+#     collection_name="milvus_docs",
+#     collection_description="All Milvus Documents",
+#     # force_new_collection=True, # If you want to drop origin collection and create a new collection every time, set force_new_collection to True
+# )
 
-question = "Write a report comparing Milvus with other vector databases."
+question = "写一篇报告，比较一下Milvus和其他向量数据"
 
 _, _, consumed_token = query(question, max_iter=1)
 print(f"Consumed tokens: {consumed_token}")
